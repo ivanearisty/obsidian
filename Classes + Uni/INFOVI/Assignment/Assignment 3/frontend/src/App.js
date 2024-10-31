4f323b434464efdf18aedc47c00dd50b70f56a73
ivanearisty <iae225@stern.nyu.edu>
2024-10-31 02:04:16 -0400

    vault backup: 2024-10-31 02:04:16

--- Classes + Uni/INFOVI/Assignment/Assignment 3/frontend/src/App.js
+++ Classes + Uni/INFOVI/Assignment/Assignment 3/frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import React from 'react';
 import Chart from './components/Graph';
+import { useState } from "react";
+import * as d3 from "d3";
 
 
 function App() {
+  
+  const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
+
+  function onMouseMove(event) {
+    const [x, y] = d3.pointer(event);
+    setData(data.slice(-200).concat(Math.atan2(x, y)));
+  }
+
   return (
-      <div className="App">
+      <div className="App" onMouseMove={onMouseMove}>
         <h1 className="text-center">Interactive Visualization</h1>
-        <Chart />
+        <Chart data={data}/>
         
       </div>
   );
